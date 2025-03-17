d5869e96834f12c81f3459f10191c787aacc3287
OutOfNet <neteoris@proton.me>
2025-03-17 12:59:31 +0100

    Removed Platform & changed scaling.
    Removed the platform in the baseplate scene and changed some settings to make the game scale better with different screen resolutions

--- Scenes/Player/player_management.gd
+++ Scenes/Player/player_management.gd
@@ -4,14 +4,15 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-export var speed = 200
-export var fall_acceleration = 750
-const jump_force = 350
+export var speed = 300
+export var fall_acceleration = 1125
+const jump_force = 400
+const base_max_fall_speed = 250
 var velocity = Vector2.ZERO
 var direction := Input.get_axis("left_input", "right_input")
 var is_mid_air = false
 var has_jumped = false
-var max_fall_speed = 250
+var max_fall_speed = 375
 
 func _input(_event):
 	if Input.is_action_pressed("left_input") == true:
@@ -21,9 +22,9 @@ func _input(_event):
 	if Input.is_action_just_pressed("jump_input"):
 		velocity.y -= jump_force
 	elif Input.is_action_just_pressed("down_input"):
-		max_fall_speed = 500
+		max_fall_speed = base_max_fall_speed * 2
 	elif Input.is_action_just_released("down_input"):
-		max_fall_speed = 250
+		max_fall_speed = base_max_fall_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -38,10 +39,10 @@ func _physics_process(delta):
 		$AnimatedSprite.play("idle")
 	elif velocity.x != 0 && velocity.y == 0:
 		$AnimatedSprite.play("run")
-	else:
+	elif velocity.y != 0:
 		$AnimatedSprite.play("mid_air")
-	if velocity.y < -250:
-		velocity.y = -250
+	if velocity.y < -jump_force:
+		velocity.y = -jump_force
 	elif velocity.y > max_fall_speed:
 		velocity.y = max_fall_speed
 	velocity.x = speed * direction
