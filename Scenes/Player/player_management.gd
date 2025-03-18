03d4ca349a218953e3b55b1e18fc1057cbe854fd
OutOfNet <neteoris@proton.me>
2025-03-18 19:19:34 +0000

    Added health.
    Added a health and damage system (the giant coin in the baseplate serves as a damage source for testing purposes), also added a visual representation of the player hitboxes, the blue one being for detecting collisions with the terrain and the green one being for detecting collisions with damage sources.

--- Scenes/Player/player_management.gd
+++ Scenes/Player/player_management.gd
@@ -8,11 +8,22 @@ export var speed = 300
 export var fall_acceleration = 1125
 const jump_force = 400
 const base_max_fall_speed = 250
+const max_health = 100
+var health = max_health
 var velocity = Vector2.ZERO
 var direction := Input.get_axis("left_input", "right_input")
 var is_mid_air = false
 var has_jumped = false
 var max_fall_speed = 375
+var is_on_cooldown = false
+var damage_cooldown = 1
+var attacks_entered = []
+
+func take_damage():
+	health -= 10
+	is_on_cooldown = true
+	print(health)
+	get_node("../damage_cooldown").start(damage_cooldown)
 
 func _input(_event):
 	if Input.is_action_pressed("left_input") == true:
@@ -26,15 +37,10 @@ func _input(_event):
 	elif Input.is_action_just_released("down_input"):
 		max_fall_speed = base_max_fall_speed
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimatedSprite.play("idle")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	direction = Input.get_axis("left_input", "right_input")
-	print(velocity.y)
-	print(velocity.x)
+	
+	# Handles player animations (please don't call me yandere dev i am NOT into kids)
 	if velocity.y == 0 && velocity.x == 0:
 		$AnimatedSprite.play("idle")
 	elif velocity.x != 0 && velocity.y == 0:
@@ -45,6 +51,25 @@ func _physics_process(delta):
 		velocity.y = -jump_force
 	elif velocity.y > max_fall_speed:
 		velocity.y = max_fall_speed
+	
+	# Handles movement
 	velocity.x = speed * direction
 	velocity.y += fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector2.ZERO)
+
+func _on_hitbox_entered_body(body):
+	if body.get_class() == "RigidBody2D" && is_on_cooldown == false:
+		attacks_entered.append(body)
+		take_damage()
+
+func _on_Timer_timeout():
+	if is_on_cooldown == true:
+		is_on_cooldown = false
+		print("damage is no longer on cooldown")
+		if attacks_entered.empty() != true:
+			take_damage()
+
+
+func _on_hitbox_exited_body(body):
+	if body.get_class() == "RigidBody2D":
+		attacks_entered.erase(body)
